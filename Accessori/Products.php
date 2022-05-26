80746ab0e4c267fb34cbd312648956f296ac2234
Aurel1701 <gantzfan96@gmail.com>
2022-05-26 13:56:55 +0200

    create Product

---
+++ Accessori/Products.php
@@ -0,0 +1,35 @@
+<?php
+/* classe padre prodotti */
+class Products {
+   protected $cibo;
+   protected $guinzaglio;
+   protected $cuccia;
+   protected $ciotola;
+   protected $prezzo;
+
+   function __construct( String $cibo,String $guinzaglio,String $cuccia,String $ciotola,String $prezzo){
+       $this->cibo = $cibo;
+       $this->guinzaglio = $guinzaglio;
+       $this->cuccia = $cuccia;
+       $this->ciotola = $ciotola;
+       $this->prezzo = $prezzo;
+   }
+   public function getCibo()
+   {
+       return $this->cibo;
+   }
+   public function getGuinzaglio()
+   {
+       return $this->guinzaglio;
+   }
+   public function getCuccia()
+   {
+       return $this->cuccia;
+   }
+   public function getCiotola()
+   {
+       return $this->ciotola;
+   }
+}
+
+?>
